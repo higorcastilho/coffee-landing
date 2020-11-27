3fabcb33ac17b85908854a9592e347a7f0c9a137
Higor Castilho <castilhohf@gmail.com>
2020-11-27 05:06:28 -0300

    refactor: change display from flex to grid

--- src/pages/ProductPrice/index.jsx
+++ src/pages/ProductPrice/index.jsx
@@ -11,8 +11,8 @@ function ProductPrice() {
 	<div id="product-price-section">
 		<main>
 			<img id="product-float-left" src={WheyCoffee} width="200" height="182" alt=""/>
-			<section>
-				<div>
+			<article>
+				<div id="text-workout-undraw">
 					<h4> + Força</h4>
 					<p>
 						A ingestão de proteínas de alto valor biológico
@@ -20,19 +20,16 @@ function ProductPrice() {
 					</p>
 				</div>
 				<img id="workout-undraw" src={Workout} width="180" height="140" alt=""/>
-			</section>
-
-			<section>
+				
 				<img id="running-undraw" src={Running} width="180" height="140" alt=""/>
-				<div>
+				<div id="text-running-undraw">
 					<h4> + Disposição</h4>
 					<p>
 						Seu Whey Coffee, além de proteínas de alto valor biológico,
-						contém também um dos melhores termogênicos estimulantes do mundo:
-						a cafeína.
+						contém também a cafeína, um dos melhores termogênicos estimulantes do mundo.
 					</p>
 				</div>
-			</section>
+			</article>
 		</main>
 	</div>
 
